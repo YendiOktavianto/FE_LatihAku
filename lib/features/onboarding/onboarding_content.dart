639b07e8cd1c6f71204b8f7d390c7b689d1321ab
LAPTOP-5PJ49EUI\user <gerhanaabiwow@gmail.com>
2022-10-30 14:59:56 +0700

    merge yendi

--- lib/features/onboarding/onboarding_content.dart
+++ lib/features/onboarding/onboarding_content.dart
@@ -1,7 +1,6 @@
 //@dart=2.9
 import 'package:fe_latihaku/configurations/size_config.dart';
 import 'package:fe_latihaku/constants/constant_colors.dart';
-import 'package:fe_latihaku/constants/constant_text_styles.dart';
 import 'package:flutter/material.dart';
 
 
